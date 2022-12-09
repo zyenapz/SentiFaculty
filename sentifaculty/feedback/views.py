fdce6cad4b96561c0dbb1c65aa2383991a591213
zyenapz <zyenapz@gmail.com>
2022-12-09 11:30:48 +0800

    Added feedback view

--- sentifaculty/feedback/views.py
+++ sentifaculty/feedback/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def feedback(request):
+    context = {
+        'title': "Feedback",
+    }
+    return render(request, 'feedback/feedback.html', context)
