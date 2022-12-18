b958a879311e40c930e2cd6297decb61ab47f6c4
Mu-Cepheus <>
2022-12-18 22:52:29 +0800

    login and logout for visualizer

--- sentifaculty/visualizer/urls.py
+++ sentifaculty/visualizer/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.visualizer_home, name="visualizer-home"),
     path('login/', auth_views.LoginView.as_view(template_name='visualizer/login.html',
-         extra_context={'title': "Visualizer"}), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+         extra_context={'title': "Visualizer login"}), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='visualizer/logout.html',
+         extra_context={'title': "Visualizer logout"}), name='logout'),
 ]
--- sentifaculty/visualizer/views.py
+++ sentifaculty/visualizer/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def visualizer_home(request):
-    return HttpResponse("testing attention please")
+    context = {
+        'title': "Visualizer dashboard",
+    }
+    return render(request, 'visualizer/visualizer_home.html', context)
