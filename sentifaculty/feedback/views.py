1dffa91b84e4c489d5ed17575a4e87b5df2f352a
zyenapz <zyenapz@gmail.com>
2023-01-04 20:46:56 +0800

    Revert "Merge pull request #5 from zyenapz/feedback-ui"
    This reverts commit b5c16a60792d72e3655a9fee8b53ede3c13f9ba3, reversing
    changes made to 23a11a39b03ccf5e70a82541ef64ca2a06931a7d.

--- sentifaculty/feedback/views.py
+++ sentifaculty/feedback/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
-from .forms import FeedbackForm
-from .models import AcademicYear
 
 # Create your views here.
 def feedback(request):
-
-    form = FeedbackForm
-
-    if request.method == "POST":
-        # print(request.POST)
-        form = FeedbackForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-
     context = {
         'title': "Feedback",
-        'form': form,
     }
-    
     return render(request, 'feedback/feedback.html', context)
