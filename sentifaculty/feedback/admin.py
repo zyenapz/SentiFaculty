a074e01bc59a322175a5453b2089914654302579
zyenapz <zyenapz@gmail.com>
2023-01-04 20:47:55 +0800

    Revert "revert"
    This reverts commit 3de1a0bd48f3f1581e7610b71abcd610030fc914.

--- sentifaculty/feedback/admin.py
+++ sentifaculty/feedback/admin.py
@@ -1,7 +1,7 @@
 from django.apps import apps
 from django.contrib import admin
 
-feedback_models = apps.get_app_config('student').get_models()
+feedback_models = apps.get_app_config('feedback').get_models()
 
 for model in feedback_models:
     admin.site.register(model)
--- sentifaculty/sentifaculty/settings.py
+++ sentifaculty/sentifaculty/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'student.apps.StudentConfig',
+    'feedback.apps.FeedbackConfig',
     'visualizer.apps.VisualizerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
