f9359c999151cd0af905e043c765da838a855304
zyenapz <zyenapz@gmail.com>
2023-01-10 14:32:48 +0800

    Moved base templates and static files to 'sentifaculty' folder so all apps can use them

--- sentifaculty/sentifaculty/settings.py
+++ sentifaculty/sentifaculty/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'sentifaculty.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['sentifaculty/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,9 +128,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'sentifaculty/static/',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'visualizer-home'
+LOGIN_REDIRECT_URL = 'feedback'
 LOGIN_URL = 'login'
